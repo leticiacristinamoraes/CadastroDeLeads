d94ea8405f140f6681265c91844ae84cd45ab7b2
Letícia Cristina França Moraes dos Santos <leticiacristinafmds@gmail.com>
2021-11-17 22:33:08 -0300

    Criar projeto

---
+++ src/main/java/br/com/zup/CadastroDeLeads/CadastroDeLeadsApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.CadastroDeLeads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeLeadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeLeadsApplication.class, args);
+	}
+
+}
